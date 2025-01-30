2598d6ff2b53e699acfe08c1845e39d266e09f56
laureschaeffer <laure.schaeffer@hotmail.com>
2025-01-30 17:27:24 +0100

    add header & navbar + css

--- view/template.php
+++ view/template.php
@@ -9,46 +9,49 @@ use Service\Message;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
     <link rel="stylesheet" href="./public/style.css">
     <title><?= $title ?? 'Home'; ?></title>
 </head>
 <body>
-    <div id="wrapper">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="index.php?action=home">Logo</a>
+    
+    <header id="header">
+        <nav id="nav">
+            <a href="index.php?action=hom" class="nav__logo">Logo</a>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <?php
-                        if(isset($_SESSION["user"])){ ?>
-                            <li class="nav-item">
-                                <a class="nav-link" href="index.php?action=chat">Chat</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="index.php?action=logout">Logout</a>
-                            </li>
-                        <?php } else { ?>
-                            <li class="nav-item">
-                                <a class="nav-link" href="index.php?action=login">Login</a>
-                            </li>
-                        <?php } ?>
-                    </ul>
+            <div class="nav__menu" id="nav-menu">
+                <ul class="nav__list">
+                    <?php
+                    // if user connected 
+                    if(isset($_SESSION["user"])){ ?>
+                        <li class="nav__item"><a class="nav__link" href="index.php?action=chat">Chat</a></li>
+                        <li class="nav__item"><a class="nav__link" href="index.php?action=logout">Logout</a></li>
+                    <?php 
+                    } else { ?>
+                        <li class="nav__item"><a class="nav__link" href="index.php?action=login">Login</a></li>
+                    <?php 
+                    } ?>
+                </ul>
+
+                <!-- close button -->
+                <div class="nav__close" id="nav-close">
+                    <i class="ri-close-line"></i>
                 </div>
             </div>
+
+            <div class="nav__toggle" id="nav-toggle">
+                <i class="ri-menu-4-line"></i>
+            </div>
         </nav>
-        
-        <?= Message::showMessage() ?>
+    </header>
 
-        <main>
-            <?= $content ?>
-        </main>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- here alert message -->
+    <?= Message::showMessage() ?>
 
+    <main>
+        <?= $content ?>
+    </main>
+
+    <script src="./public/main.js"></script>
 </body>
 </html>
