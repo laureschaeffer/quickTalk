c8f3f0ab71a42b57acc12348a8ba0f371db47615
laureschaeffer <laure.schaeffer@hotmail.com>
2025-02-03 13:08:12 +0100

    add a feature section

--- view/home.php
+++ view/home.php
@@ -1,34 +1,85 @@
-<section id="home">
+<div id="intro">
     <div class="home__shape" id="shape-1"></div>
     <div class="home__shape" id="shape-2"></div>
     <div class="home__shape" id="shape-3"></div>
+    <img src="./public/img/shape-bg.png" alt="shape background" class="home__shape-bg">
 
-    <div class="home__info">
-        <div class="home__titles">
-            <h1>Instant messaging</h1>
-            <h2>Secure and fast</h2>
+    <div id="intro-container">
+        <div class="home__info">
+            <div class="home__titles">
+                <h1>Instant messaging</h1>
+                <h2>Secure and fast</h2>
+            </div>
+            <span class="home__description">
+                Experience seamless communication with end-to-end encryption and ultra-fast delivery, ensuring your messages are always private and instant.
+            </span>
+    
+            <div class="home__data">
+                <div>
+                    <h3 class="home__data-number">26K</h3>
+                    <span class="home__data-subtitle">Messages sent</span>
+                </div>
+                <div>
+                    <h3 class="home__data-number">5K</h3>
+                    <span class="home__data-subtitle">Members</span>
+                </div>
+                <div>
+                    <h3 class="home__data-number">3</h3>
+                    <span class="home__data-subtitle">years</span>
+                </div>
+            </div>
         </div>
-        <span class="home__description">
-            Experience seamless communication with end-to-end encryption and ultra-fast delivery, ensuring your messages are always private and instant.
-        </span>
-
-        <div class="home__data">
-            <div>
-                <h3 class="home__data-number">26K</h3>
-                <span class="home__data-subtitle">Messages sent</span>
-            </div>
-            <div>
-                <h3 class="home__data-number">5K</h3>
-                <span class="home__data-subtitle">Members</span>
-            </div>
-            <div>
-                <h3 class="home__data-number">3</h3>
-                <span class="home__data-subtitle">years</span>
-            </div>
-        </div>
+        <figure>
+            <img src="./public/img/chat.png" alt="chatting picture" class="home-img">
+        </figure>
     </div>
-    <figure>
-        <img src="./public/img/chat.png" alt="chatting picture" class="home-img">
-    </figure>
 
+</div>
+
+<section id="features">
+    <h4>Features</h4>
+
+    <div class="feature__card-container">
+
+        <div class="feature__card">
+            <figure>
+                <img src="./public/img/bubble.png" alt="feature bubble image" class="feature__card-img">
+            </figure>
+            <p class="feature__card-title">Title</p>
+            <span class="feature__card-content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis blanditiis modi quod dolore cum. Sapiente mollitia aut deleniti voluptatibus eligendi
+            </span>
+        </div>
+        
+        <div class="feature__card">
+            <figure>
+                <img src="./public/img/bubble.png" alt="feature bubble image" class="feature__card-img">
+            </figure>
+            <p class="feature__card-title">Title</p>
+            <span class="feature__card-content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis blanditiis modi quod dolore cum. Sapiente mollitia aut deleniti voluptatibus eligendi
+            </span>
+        </div>
+
+        <div class="feature__card">
+            <figure>
+                <img src="./public/img/bubble.png" alt="feature bubble image" class="feature__card-img">
+            </figure>
+            <p class="feature__card-title">Title</p>
+            <span class="feature__card-content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis blanditiis modi quod dolore cum. Sapiente mollitia aut deleniti voluptatibus eligendi
+            </span>
+        </div>
+
+        <div class="feature__card">
+            <figure>
+                <img src="./public/img/bubble.png" alt="feature bubble image" class="feature__card-img">
+            </figure>
+            <p class="feature__card-title">Title</p>
+            <span class="feature__card-content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis blanditiis modi quod dolore cum. Sapiente mollitia aut deleniti voluptatibus eligendi
+            </span>
+        </div>
+
+    </div>
 </section>
